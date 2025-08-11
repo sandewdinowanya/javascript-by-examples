fdc92e0078080438b2a6d128f20bdb9be3c2799b
Sandewdi Nowanya <nowanyafonseka@gmail.com>
2025-08-11 22:49:11 +0530

    feat: implement click event to update counter value

---
+++ click-counter /script.js
@@ -0,0 +1,9 @@
+let count = document.getElementById("add");
+let btn = document.getElementById("click");
+let increaseCount = 0;
+
+// Add event listner
+btn.addEventListener("clickbtn",function(){
+  increasecount++;
+  count.textContent=increascount;
+});
