a666f372421b9f2e004bdf9be5f4c9a80920a7d0
Sandewdi Nowanya <nowanyafonseka@gmail.com>
2025-08-13 08:30:57 +0530

    chore: delete invalid file path 'click-counter/index.html'

--- click-counter /script.js
+++
@@ -1,9 +0,0 @@
-let count = document.getElementById("count");
-let btn = document.getElementById("add");
-let increaseCount = 0;
-
-// Add event listner
-btn.addEventListener("click",function(){
-  increaseCount++;
-  count.textContent=increaseCount;
-});
