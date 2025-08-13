80464ecc58a0a97f1c9c88d011714edf29f5285b
Sandewdi Nowanya <nowanyafonseka@gmail.com>
2025-08-14 00:14:00 +0530

    refactor: improve click counter structure and styling

--- click-counter/script.js
+++ click-counter/script.js
@@ -1,9 +1,9 @@
 let value = document.getElementById("counter");
-let button = document.getElementById("button");
+let button = document.getElementById("add");
 
 let count = 0;
 
-button.addEventListener("click", function(){
+button.addEventListener("click", function () {
     count++;
     value.textContent = count;
 });
