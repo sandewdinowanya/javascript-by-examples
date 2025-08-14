13786f10f5a50086bf9d7074c0465739156a0202
Sandewdi Nowanya <nowanyafonseka@gmail.com>
2025-08-14 23:46:18 +0530

    feat: add start/stop interval-based background color changer with manual override

--- click-counter/background-color-changer/script.js
+++ click-counter/background-color-changer/script.js
@@ -1,5 +1,10 @@
-let colorbtn = document.getElementById("colorbtn");
+// let colorbtn = document.getElementById("colorbtn");
 let colorvalue = document.getElementById("colorvalue");
+
+let startbutton = document.getElementById("startbutton");
+let endbutton = document.getElementById("endbutton"); 
+
+let intervalId = null;
 
 function getRandomColor (){
     let letters = "012345678ABCDEF";
@@ -12,6 +17,21 @@ function getRandomColor (){
     }
     return color;
 };
+
+startbutton.addEventListener("click",function(){
+    if(intervalId === null){
+        intervalId = setInterval(function(){
+            let newColor=getRandomColor();
+            document.body.style.backgroundColor = newColor;
+            colorvalue.textContent = newColor;
+        }, 1000)
+    }
+});
+
+endbutton.addEventListener("click",function(){
+    clearInterval(intervalId);
+    intervalId = null;
+});
 
 colorbtn.addEventListener("click", function(){
     let newColor = getRandomColor();
