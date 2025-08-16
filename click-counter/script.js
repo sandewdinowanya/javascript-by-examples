7bfc0bcb66c4674ae5a9f5b0ef45fee99d4051e4
Sandewdi Nowanya <nowanyafonseka@gmail.com>
2025-08-16 21:56:29 +0530

    feat: implement background color change on click counter

--- click-counter/script.js
+++ click-counter/script.js
@@ -7,8 +7,29 @@ let count = 0;
 add.addEventListener("click", function () {
     count++;
     value.textContent = count;
+    let color = getRandomColor();
+    document.body.style.backgroundColor = color;
 });
 
 reset.addEventListener("click",function(){
-    value.textContent = 0;
+    count = 0;
+    value.textContent = count;
+    document.body.style.backgroundColor = "#FFFFFF";
 });
+
+
+function getRandomColor(){
+    let colorValue = "#";
+    let value = "012345678ABCDEF";
+
+    for(let i=0; i<6; i++){
+        colorValue += value[Math.floor(Math.random()*16)];
+    }
+    return colorValue;
+}
+
+add.addEventListener("click", function(){
+        let color = getRandomColor();
+        document.body.style.backgroundColor = color;
+});
+
