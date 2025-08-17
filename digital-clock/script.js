c91b98b2dafbad1fd4211f2ccd26ff5c40b74738
Sandewdi Nowanya <nowanyafonseka@gmail.com>
2025-08-17 23:18:33 +0530

    feat: add day and date display with color change

--- digital-clock/script.js
+++ digital-clock/script.js
@@ -1,5 +1,6 @@
 let clock = document.getElementById("clock");
 let format = document.getElementById("format");
+let date = document.getElementById("day-and-date");
 
 function updateClock(){
     let now = new Date();
@@ -7,7 +8,7 @@ function updateClock(){
     let minutes = now.getMinutes().toString().padStart(2,"0");
     let hours = now.getHours();
     clock.textContent = `${minutes}:${seconds}:${format.textContent}`;
-    
+
     let period = hours >= 12 ? "PM" : "AM";
 
     // Convert to 12-hour format
@@ -21,6 +22,28 @@ function updateClock(){
     format.textContent = period;
 };
 
+function changeColor(){
+    let color = "#"
+    let value = "012345678ABCDEF";
+
+    for(let i = 0; i < 6; i++){
+        color += value[Math.floor(Math.random() * 16)];
+    }
+    document.body.style.backgroundColor = color;
+};
+
+function getDate(){
+    let dayAndDate = new Date();
+    date.textContent = dayAndDate.toLocaleDateString("en-US", {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    });
+};
+
 setInterval(updateClock,1000);
+setInterval(changeColor, 5000);
 
 updateClock();
+getDate();
