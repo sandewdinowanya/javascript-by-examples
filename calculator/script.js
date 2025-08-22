648470d881ce1b2e443fa58afeadccac08f8fab2
Sandewdi Nowanya <nowanyafonseka@gmail.com>
2025-08-22 10:16:36 +0530

    feat: implement basic calculator functionality

---
+++ calculator/script.js
@@ -0,0 +1,26 @@
+function  calculate(operator){
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let result;
+
+    if(isNaN(num1) || isNaN(num2)){
+        result = "Please enter both numbers!";
+    }else{
+        switch(operator){
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num2 !== 0 ? (num1 / num2) : "Can't divide by 0";
+                break;
+        }
+    }
+
+    document.getElementById("result").textContent = result;
+}
