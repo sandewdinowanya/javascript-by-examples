47a9b908732fe6bc1886ee253af2d8a7fe1290c1
Sandewdi Nowanya <nowanyafonseka@gmail.com>
2025-08-27 23:59:01 +0530

    feat: implement clear button functionality

--- calculator/script.js
+++ calculator/script.js
@@ -1,7 +1,18 @@
 function  calculate(operator){
-    let num1 = parseFloat(document.getElementById("num1").value);
-    let num2 = parseFloat(document.getElementById("num2").value);
+    let num1field = document.getElementById("num1");
+    let num2field = document.getElementById("num2");
+    let clear = document.getElementById("clear");
     let result;
+
+    let num1 = parseFloat(num1field.value);
+    let num2 = parseFloat(num2field.value);
+
+    if(operator === clear){ 
+        num1field.value = '';
+        num2field.value = '';
+        document.getElementById("result").textContent = '';
+        return; 
+    }
 
     if(isNaN(num1) || isNaN(num2)){
         result = "Please enter both numbers!";
@@ -18,9 +29,10 @@ function  calculate(operator){
                 break;
             case '/':
                 result = num2 !== 0 ? (num1 / num2) : "Can't divide by 0";
-                break;
+                break;   
         }
     }
 
     document.getElementById("result").textContent = result;
-}
+
+} 
