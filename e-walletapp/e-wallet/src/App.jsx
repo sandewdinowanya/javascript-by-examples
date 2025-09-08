dcae371a6929203a658687e715445bd15ec5c73f
Sandewdi Nowanya <nowanyafonseka@gmail.com>
2025-09-08 23:53:22 +0530

    feat: implement basic login form ui

--- e-walletapp/e-wallet/src/App.jsx
+++ e-walletapp/e-wallet/src/App.jsx
@@ -1,52 +1,44 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {Eye, EyeOff} from 'lucide-react'
 import './App.css'
 
 function App() {
-  
-    return (
-        <>
-        <div>
-            <label className="login-title">Email</label>
-            <input type="email"
-            classname="email"
-            placeholder="Enter your email">
-            </input>
-        </div>
+  return (
+    <div className="login-container">
+      <div>
+        <label className="login-title">Email</label>
+        <input
+          type="email"
+          className="email"
+          placeholder="Enter your email"
+        />
+      </div>
 
-        <div>
-            <label className="password=title">Password</label>
-            <input type="password"
-            className="password"
-            placeholder="Enter your password">
-            </input>
-        </div>
+      <div>
+        <label className="password-title">Password</label>
+        <input
+          type="password"
+          className="password"
+          placeholder="Enter your password"
+        />
+      </div>
 
-        <button className="button">
-            Show
-        </button>
+      <button className="button">Show</button>
+      <button className="login-button">Login</button>
+      <button className="google-button">Google</button>
 
-        <button className="login-button">
-            Login
-        </button>
+      <div>
+        <a className="forgot-password" href="#">
+          Forgot Password
+        </a>
+      </div>
 
-        <button className="google-button">
-          Google
-        </button>
-
-        <div>
-          <a className="forgot-password" href="#">
-            Forgot Password
-          </a>
-        </div>
-
-        <div className="sign-up">
-          <p> Don't have an account?</p>
-          <button className="sign-up">Sign Up</button>
-        </div>
-        </>
-    )
+      <div className="sign-up">
+        <p>Don't have an account?</p>
+        <button>Sign Up</button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
